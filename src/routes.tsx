e9ce75a479adbc5ed79151a9dbd8504d01e74355
Vicente Nascimento <49836207+Vicenteefenequis@users.noreply.github.com>
2020-09-23 08:23:11 -0300

    perf: change code add routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,6 +6,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Landing from './pages/Landing'
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
 
 const AppStack = createStackNavigator();
 
@@ -15,6 +17,8 @@ function Routes(){
             <AppStack.Navigator screenOptions={{headerShown:false}}>
               <AppStack.Screen name="Landing" component={Landing}/>
               <AppStack.Screen name="Login" component={Login}/>
+              <AppStack.Screen name="Home" component={Home}/>
+              <AppStack.Screen name="Register" component={Register}/>
             </AppStack.Navigator>
         </NavigationContainer>
     )
