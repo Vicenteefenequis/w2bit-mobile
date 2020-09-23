1f4966b4b174d2e40a6972f0879e75c8b3030abf
Vicente Nascimento <49836207+Vicenteefenequis@users.noreply.github.com>
2020-09-23 20:57:57 -0300

    style(Login): add styling of login user on application

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,67 @@
-import React, { useState, useEffect } from "react";
-import { View, Image, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Image, Text} from "react-native";
 import {useNavigation} from "@react-navigation/native";
+import { Input } from 'react-native-elements';
+import { RectButton } from "react-native-gesture-handler";
 
-
-import {Container} from './styles'
-
+import {
+    Container,
+    ImageContainer,
+    ContainerInputs,
+    ContainerInfo,
+    TextRegisterAccount,
+    TextNotHaveAccount,
+    Button,
+    TextButton,
+} from './styles'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 
 function Login(){
     const {navigate} = useNavigation();
+    const [password,setPassword] = useState(false);
+
+    function handleNavigateToRegister(){
+
+    }
+
+
     return (
         <Container>
-            <Text>LOGIN</Text>
+            <ImageContainer>
+                <Image  source={require('../../assets/images/login.png')}/>
+            </ImageContainer>
+            <ContainerInputs>
+                <Input placeholder="Nome"  />
+                <Input  placeholder="Password" secureTextEntry={!password} rightIcon={
+                password ? 
+                <Icon  
+                    name='unlock'
+                    size={24}
+                    color='#485068'
+                    onPress={()=> {setPassword(!password)} }
+                /> : 
+                <Icon name='lock'
+                    size={24}
+                    color='#485068'
+                    onPress={()=> {setPassword(!password)} }
+                />
+                }/>
+            </ContainerInputs>
+            <Button color="#007AFE">
+                <TextButton style={{color:"#FFFFFF"}}>Login</TextButton>
+            </Button>
+            <ContainerInfo>
+                 <TextNotHaveAccount color="#485068">
+                        Não tem conta?  {"  "}
+                    </TextNotHaveAccount>   
+                <RectButton onPress={handleNavigateToRegister}>
+                    <TextRegisterAccount color="#347AF0" > 
+                         Registre-se
+                    </TextRegisterAccount>
+                </RectButton>              
+            </ContainerInfo>
         </Container>
     )
 }
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,7 +1,52 @@
 import styled from 'styled-components/native';
 
+interface Props{
+    color?:string;
+}
+
 export const Container = styled.View`
     flex:1;
-    justify-content:center;
     align-items:center;
+    justify-content:space-around;
+`;
+
+export const ImageContainer = styled.View`
+    margin-top:64px;
+    width:100%;
+    align-items:center;
+`;
+
+export const ContainerInputs = styled.View`
+     width:80%;
+`;
+
+export const ContainerInfo = styled.View`
+    flex-direction:row;
+    align-items:center;
+`
+
+export const TextRegisterAccount = styled.Text<Props>`
+    color: ${props => props.color};
+    font-family:"TitilliumWeb_600SemiBold";
+    font-size:18px;
+`
+export const TextNotHaveAccount = styled.Text<Props>`
+    color: ${props => props.color};
+    font-size:15px;
+    opacity:0.7;
+`
+
+
+export const Button = styled.TouchableOpacity<Props>`
+    background-color:${props => props.color};
+    margin-bottom:42px;
+    width:80%;
+    padding:12px;
+    align-items:center;
+    border-radius:100px;
+`;
+
+export const TextButton = styled.Text`
+    color:white;
+    font-family:"TitilliumWeb_600SemiBold";
 `;
