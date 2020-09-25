9f2ebef789df202c13aeb245db7725ceda3810b2
Vicente Nascimento <49836207+Vicenteefenequis@users.noreply.github.com>
2020-09-25 01:30:24 -0300

    feat create context for routes of application

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,19 +1,17 @@
 import React, { createContext , useState , useEffect, useContext } from 'react'
-import {View,ActivityIndicator} from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage'
 import * as auth from '../services/auth'
-
+import {firebase} from '../../src/services/firebase'
 //import api from '../services/api'
 
 interface User{
-    email:string;
-    name:string;
+    email?:string | null;
 }
 
 interface AuthContextData  {
     signed: boolean;
     user: User | null;
-    signIn():Promise<void>;
+    signIn(email:string,password:string):Promise<void>;
     signOut(): void;
 }
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -24,9 +22,8 @@ export const AuthProvider: React.FC = ({children}) => {
     useEffect(()=>{
         async function loadStoragedData(){
             const storagedUser = await AsyncStorage.getItem('@RNAuth:user')
-            const storagedToken = await AsyncStorage.getItem('@RNAuth:token');
 
-            if(storagedToken && storagedUser){
+            if(storagedUser){
                 //api.defaults.headers['Authorization'] = `Bearer ${storagedToken}`;
                 setUser(JSON.parse(storagedUser));
             }
@@ -34,15 +31,16 @@ export const AuthProvider: React.FC = ({children}) => {
         loadStoragedData();
     },[])
 
-    async function signIn(){
-       const response = await auth.signIn();
+    async function signIn(email:string,password:string){
+        
+       const response = await auth.signIn(email,password);
 
         setUser(response.user);
 
         //api.defaults.headers['Authorization'] = `Bearer ${response.token}`;
 
         await AsyncStorage.setItem('@RNAuth:user',JSON.stringify(response.user))
-        await AsyncStorage.setItem('@RNAuth:token',response.token);
+       // await AsyncStorage.setItem('@RNAuth:token',response.token);
     }
     function signOut(){
         AsyncStorage.clear().then(()=>{
