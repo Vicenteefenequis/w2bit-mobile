8ddadf9bb139982b092a3bdc15cff947a83b9ced
Vicente Nascimento <49836207+Vicenteefenequis@users.noreply.github.com>
2020-09-25 01:31:39 -0300

    style(Theme): create themeProvider

---
+++ src/styles/default.ts
@@ -0,0 +1,25 @@
+import { DefaultTheme } from 'styled-components';
+
+const defaultTheme: DefaultTheme = {
+  title: 'default',
+
+  colors: {
+    blue: '#007AFE' ,
+    background: '#312e38',
+    white: '#f4ede8',
+    orange: '#FF9000',
+    grayHard: '#666360',
+    gray: '#999591',
+    inputs: '#3D4C63',
+    shape: '#3E3B47',
+    blackMedium: '#28262E',
+    error: '#c53030',
+  },
+
+  fonts: {
+    regular: 'TitilliumWeb_400Regular',
+    medium: 'TitilliumWeb_600SemiBold',
+  },
+};
+
+export default defaultTheme;
---
+++ src/styles/styles.d.ts
@@ -0,0 +1,26 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    title: string;
+
+    colors: {
+      background: string;
+      blue:string;
+      white: string;
+      orange: string;
+      grayHard: string;
+      gray: string;
+      inputs: string;
+      shape: string;
+      blackMedium: string;
+
+      error: string;
+    };
+
+    fonts: {
+      regular: string;
+      medium: string;
+    };
+  }
+}
